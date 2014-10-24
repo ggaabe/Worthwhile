3d47b13d8ae5c0d170a9cd825b7f8834c39371c3
ColonyOne <colonyFrontier@gmail.com>
2014-10-24 11:51:29 -0400

    Initial Commit

---
+++ Worthwhile 3.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Worthwhile 3.0
+//
+//  Created by Gabe Garrett on 10/24/14.
+//  Copyright (c) 2014 Meaningful Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
